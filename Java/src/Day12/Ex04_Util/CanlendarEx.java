12b5a5bf81f70fe1d295b2757e97c976aec6cffb
zz909 <zz909@naver.com>
2024-02-19 17:29:33 +0900

    20240219/Object/Wrapper/Lang/Util 클레스 메소드활용

---
+++ Java/src/Day12/Ex04_Util/CanlendarEx.java
@@ -0,0 +1,77 @@
+package Day12.Ex04_Util;
+
+import java.util.Calendar;
+
+public class CanlendarEx {
+
+	// 날씨/시간 정보(Calendar)를 출력하는 메소드
+	public static void printCalendar(String msg, Calendar cal){
+		
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH) + 1; // 월(0~11)
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK); // 일~토(1~7)
+		int hour = cal.get(Calendar.HOUR); // 12시 표시
+		int hourOfDay = cal.get(Calendar.HOUR_OF_DAY); // 24시 표시
+		int ampm = cal.get(Calendar.AM_PM);
+		int minute = cal.get(Calendar.MINUTE);
+		int second = cal.get(Calendar.SECOND);
+		int millisecond = cal.get(Calendar.MILLISECOND);
+		System.out.print(msg + " : " + year + "/" + month + "/" + day + "/");
+	
+		// 일요일(1) ~ 토요일(7)
+		String Day = "";
+		switch (dayOfWeek) {
+			case Calendar.SUNDAY:		Day = "일요일"; break;
+			case Calendar.MONDAY:		Day = "월요일"; break;
+			case Calendar.TUESDAY:		Day = "화요일"; break;
+			case Calendar.WEDNESDAY:	Day = "수요일"; break;
+			case Calendar.THURSDAY:		Day = "목요일"; break;
+			case Calendar.FRIDAY:		Day = "금요일"; break;
+			case Calendar.SATURDAY:		Day = "토요일"; break;
+		}
+		System.out.print(Day);
+		System.out.print("(" + hourOfDay + "시)");
+		if( ampm == Calendar.AM )
+			System.out.print("오전 ");
+		if( ampm == Calendar.PM )
+			System.out.print("오후 ");
+		
+		System.out.println(hour + "시 " + minute + "분 " + second + "초 " + millisecond + "밀리초");
+	}
+
+	public static void main(String[] args) {
+         // * new Calendar() 대신, getInstance()메소드로 객체를 사용한다
+		Calendar now = Calendar.getInstance();
+		printCalendar("현재", now);
+		
+		
+		// 자바 프로그래밍 마지막 수업일
+		Calendar test = Calendar.getInstance();
+		test.set(2024, Calendar.FEBRUARY,27);
+		printCalendar("자바 마지막 수업일", test);
+		
+		test.set(2024, Calendar.FEBRUARY,27,18,30,0);
+		printCalendar("자바 마지막 수업일(년~초)", test);
+		
+		//종강일
+		Calendar lastDay = Calendar.getInstance();
+		lastDay.set(2024, Calendar.AUGUST,1,18,0,0);
+		
+		//-lastDay  : 종강일
+		//-now      :오늘(날짜/시간)
+		long lastT = lastDay.getTimeInMillis();
+		long nowT = now.getTimeInMillis();
+		
+		//0.001초 :1ms
+		//60초    :1분
+		//60분    :1시간
+		//24시    :1일
+		System.out.println("lastT - nowT = " + (lastT - nowT));
+		
+		long dDAY = (lastT - nowT)/ (1000 * 60 * 60 * 24);
+		System.out.println("D - day  : " + dDAY);
+		}
+}
+
+
